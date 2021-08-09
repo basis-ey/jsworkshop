e07412e352e73a1250ae52898ac393ffa31eeb61
eun20 <78005066+eun20@users.noreply.github.com>
2021-08-09 17:17:15 +0900

    C2 Exercises

---
+++ idman.js
@@ -0,0 +1,5 @@
+var frameworksList = document.getElementById('frameworks');
+var newFramework = document.createElement('li');
+newFramework.innerText = "Apache Royale";
+frameworksList.appendChild(newFramework); 
+frameworksList.lastChild.style.color = "crimson"; 
