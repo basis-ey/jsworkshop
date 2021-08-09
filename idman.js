a6daa547f24cc447d7de55ae6c7426614b8bd202
eun20 <78005066+eun20@users.noreply.github.com>
2021-08-09 17:15:33 +0900

    Delete idman.js

--- idman.js
+++
@@ -1,5 +0,0 @@
-var frameworksList = document.getElementById('frameworks');
-var newFramework = document.createElement('li');
-newFramework.innerText = "Apache Royale";
-frameworksList.appendChild(newFramework); 
-frameworksList.lastChild.style.color = "crimson"; 
